ee6e112fc4ba88f604eb208ff47241ddccc017cf
murilinhops <murilo.pescatori@nubank.com.br>
2023-05-11 14:16:57 -0300

    Add aula3

---
+++ src/estudos/aula3.clj
@@ -0,0 +1,30 @@
+(ns estudos.aula3)
+
+(defn apply-discount-if?
+  [price]
+  (if (> price 90)
+    true
+    false))
+
+(defn apply-discount-when?
+  [price]
+  (when (> price 90)
+    true))
+
+(defn price-greater-than-90?
+  [price]
+  (> price 90))
+
+(def price-greater-than-90-anon? (fn [price] (> price 90)))
+
+(def price-greater-than-90-lambda? #(> % 90))
+
+(defn new-discount
+  "Retorna o valor do produto com desconto de 10% se o valor for maior que 100"
+  [apply-discount? value]
+  (if (apply-discount? value)
+    (let [discount-percentage (/ 10.0 100.0)
+          discount-value (* value discount-percentage)]
+      (println "Calculando o desconto de -" discount-value "%")
+      (- value  discount-value))
+    value))
